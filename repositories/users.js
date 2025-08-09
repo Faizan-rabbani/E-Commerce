2ee2d1a930beb8d9896a1ddc758a499ecddf8f44
Faizan Rabbani <faizanrabbani987@gmail.com>
2025-08-09 19:35:25 +0500

    new updates

---
+++ repositories/users.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Filename is required to create a repository");
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: "utf8" })
+    );
+  }
+
+  async create(attrs){
+    const records = await this.getAll();
+    records.push(attrs)
+
+    await this.writeAll(records)
+  }
+
+  async writeAll(records){
+    await fs.promises.writeFile(this.filename, JSON.stringify(records,null,2))
+  }
+}
+
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  await repo.create({ name: "John Doe", email: "johndoe@example.com"})
+  const user = await repo.getAll();
+  console.log(user);
+};
+
+test();
