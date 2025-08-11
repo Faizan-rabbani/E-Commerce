bc466ad9f359b25931d713fbf9de7ca7ae243ee1
Faizan Rabbani <faizanrabbani987@gmail.com>
2025-08-11 18:55:26 +0500

    new updates

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieSession({keys: ['key1']}))
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(`
         <div>
             <form method="POST">
                 <input name= "email" placeholder= "email" />
                 <input name= "password" placeholder= "password" />
                 <input name= "passwordconfirmation" placeholder= "confirm Password" />
-                <button>Submit</button>
+                <button>Sign up</button>
             </form>
         </div>
         `);
 });
 
-app.post('/',async(req, res) => {
+app.post('/signup',async(req, res) => {
   const {email, password, passwordconfirmation} = req.body;
 
   const existedUser = await repo.getOneBy({email})
@@ -38,6 +38,37 @@ app.post('/',async(req, res) => {
 
     res.send("Account Created")
 })
+
+app.get('/signout', (req,res) => {
+  req.session = null
+  res.send("You are logged out")
+})
+
+app.get('/signin', (req,res) => {
+  res.send(`
+     <div>
+        <form method="POST">
+          <input name= "email" placeholder= "email" />
+          <input name= "password" placeholder= "password" />
+          <button>Sign in</button>
+          </form>
+      </div>
+        `)
+})
+
+app.post('/signin', async(req,res) => {
+  const {email, password} = req.body
+  const user = await repo.getOneBy({email})
+  if(!user){
+    return res.send("Email not found")
+  }
+  if(user.password !== password){
+    return res.send("Password is incorrect")
+  }
+  req.session.user = user.id
+  res.send("You are logged in")
+})
+ 
 
 app.listen(3000, () => {
   console.log("listening");
