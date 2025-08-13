6952e4dbeb84021710092f7126067fff24cd12ea
Faizan Rabbani <faizanrabbani987@gmail.com>
2025-08-13 15:16:25 +0500

    new updates

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,14 @@
+import layout from "../layout.js"
 export default () => {
-    return `
-    <div>
-        <form method="POST">
-          <input name= "email" placeholder= "email" />
-          <input name= "password" placeholder= "password" />
-          <button>Sign in</button>
-          </form>
-      </div>
-      `
+    return layout({
+        content: `
+            <div>
+                <form method="POST">
+                    <input name= "email" placeholder= "email" />
+                    <input name= "password" placeholder= "password" />
+                    <button>Sign in</button>
+                </form>
+            </div>
+    ` 
+    })
 }
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,12 +1,15 @@
+import layout from "../layout.js"
 export default () => {
-    return `
-    <div>
-            <form method="POST">
-                <input name= "email" placeholder= "email" />
-                <input name= "password" placeholder= "password" />
-                <input name= "passwordconfirmation" placeholder= "confirm Password" />
-                <button>Sign up</button>
-            </form>
-        </div>
-        `
+    return layout({
+        content: `
+            <div>
+                <form method="POST">
+                    <input name= "email" placeholder= "email" />
+                    <input name= "password" placeholder= "password" />
+                    <input name= "passwordconfirmation" placeholder= "confirm Password" />
+                    <button>Sign up</button>
+                </form>
+            </div>
+       `
+    }) 
 }
